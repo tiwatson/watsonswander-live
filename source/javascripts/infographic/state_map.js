640c94d2ced12deb27cb4b0b895d5fe52751556f
Tim Watson <tiwatson@gmail.com>
2015-02-07 15:29:01 -0500

    Places bubbles v1

--- source/javascripts/infographic/state_map.js
+++ source/javascripts/infographic/state_map.js
@@ -21,6 +21,7 @@ var svg = state_map_div.append("svg")
 
 states = svg.append("svg:g").attr("id", "states");
 track = svg.append("svg:g").attr("id", "track");
+points = svg.append("svg:g").attr("id", "points");
 
 d3.json("/states.json", function(json) {
   states.selectAll("path")
@@ -64,6 +65,16 @@ function trackPlay() {
   .delay(1000)
   .duration(25000);
 
+// d3.select(points.selectAll(".point")[0][100]).style('fill-opacity', 0.5).style('stroke-opacity', 1)
+
+// points.selectAll(".point").transition().ease('linear')
+//   .delay(1000)
+//   .duration(25000).tween("nil", function(d,i) {
+//     var ii = d3.interpolateRound(0, 100);
+//     console.log(i,ii);
+//   d3.select(points.selectAll(".point")[0][i]).style('fill-opacity', 0.5).style('stroke-opacity', 1)
+// });
+
 }
 
 d3.select('.replayBox').on('click', function() {
@@ -78,37 +89,22 @@ d3.select('.buttonTrack').on('click', function() {
   d3.selectAll('.stateMapButtonContent').classed('hidden', true);
   d3.select('.contentTrack').classed('hidden', false);
 
+  points.selectAll(".point").transition().style('fill-opacity', 0).style('stroke-opacity', 0).duration(1000);
   trackPlay();
   d3.event.stopPropagation();
+
+
 });
 
-d3.select('.buttonDays').on('click', function() {
+d3.select('.buttonPoints').on('click', function() {
   d3.selectAll('.btn').classed('active', false);
   d3.select(this).classed('active', true);
 
   d3.selectAll('.stateMapButtonContent').classed('hidden', true);
   d3.select('.contentDays').classed('hidden', false);
 
-  track.selectAll("path").transition().duration(1000).style('opacity', 0);
-
-  d3.selectAll('.states')
-  .transition().duration(1000)
-  .style("fill-opacity", function(d) { return d.properties.days; });
-  d3.event.stopPropagation();
-});
-
-d3.select('.buttonCostPerDay').on('click', function() {
-  d3.selectAll('.btn').classed('active', false);
-  d3.select(this).classed('active', true);
-
-  d3.selectAll('.stateMapButtonContent').classed('hidden', true);
-  d3.select('.contentCostPerDay').classed('hidden', false);
-
-  track.selectAll("path").transition().duration(1000).style('opacity', 0);
-
-  d3.selectAll('.states')
-  .transition().duration(1000)
-  .style("fill-opacity", function(d) { return d.properties.cost_per_day; });
+  track.selectAll("path").transition().duration(1000).style('opacity', 0.15).attr("stroke-dashoffset", 0);
+  points.selectAll(".point").transition().style('fill-opacity', 0.5).style('stroke-opacity', 1).duration(1000);
   d3.event.stopPropagation();
 });
 
@@ -142,37 +138,42 @@ var state_map = function (data) {
   .attr("x", function(d, i) { return projection([d.longitude, d.latitude])[0] + 3; })
   .attr("y", function(d, i) { return projection([d.longitude, d.latitude])[1] + 12; });
 
-  var costPerDayMin = d3.min(data.states.map( function(d) { return d.cost_per_day; }));
-  var costPerDayMax = d3.max(data.states.map( function(d) { return d.cost_per_day; }));
-  var costPerDayScale = d3.scale.linear().domain([costPerDayMin,costPerDayMax]).range([0.1,0.99]);
+  var stayLengthMin = d3.min(data.map_places.map( function(d) { return d.stay_length; }));
+  var stayLengthMax = d3.max(data.map_places.map( function(d) { return d.stay_length; }));
+  var stayLengthScale = d3.scale.linear().domain([stayLengthMin,stayLengthMax]).range([4,25]);
 
-  var daysMin = d3.min(data.states.map( function(d) { return d.days; }));
-  var daysMax = d3.max(data.states.map( function(d) { return d.days; }));
-  var daysScale = d3.scale.linear().domain([daysMin,daysMax]).range([0.1,0.99]);
-
-
-  data.states.forEach(function(s) {
-    d3.select(".state-" + s.state.toLowerCase() ).data()[0].properties.cost_per_day = costPerDayScale(s.cost_per_day);
-    d3.select(".state-" + s.state.toLowerCase() ).data()[0].properties.days = daysScale(s.days);
+  map_places = [];
+  data.map_places.forEach( function(place) {
+    map_places.push({
+      type: "Feature",
+      properties: {
+        title: place.title,
+        city_state: place.city + ", " + place.state_short,
+        stay_length: place.stay_length
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          place.longitude,
+          place.latitude
+        ]
+      }
+    });
   });
 
-  d3.selectAll('.states')
-  .style('fill', function(d) {
-    if (typeof d.properties.days !== 'undefined') {
-      return "rgba(43, 84, 126, 1)";
-    }
-    else {
-      return "rgba(204, 204, 204, 1)";
-    }
-  })
-  .style("fill-opacity", function(d) {
-    if (typeof d.properties.days !== 'undefined') {
-      return 0.5;
-    }
-    else {
-      return 1;
-    }
-  });
+  points.selectAll("circle")
+    .data(map_places.sort(function(a, b) { return b.properties.stay_length - a.properties.stay_length; }))
+    .enter().append("circle")
+    .attr("d", path)
+    .attr("stroke", "black")
+    .attr('class', 'point')
+    .attr("cx", function(d, i) { return projection(d.geometry.coordinates)[0]; })
+    .attr("cy", function(d, i) { return projection(d.geometry.coordinates)[1]; })
+    .attr("r", function(d, i) { return stayLengthScale(d.properties.stay_length); })
+    .append("title")
+      .text(function(d) {
+        return d.properties.title + "\n" + d.properties.city_state + "\n" + d.properties.stay_length + " Nights";
+      });
 
   trackPlay();
 
