567e1a440e1c820b3a5e4e3dc8eac7c9e764e07c
Tim Watson <tiwatson@gmail.com>
2015-02-09 18:39:44 -0500

    Update categories colors

--- source/javascripts/infographic/categories.js
+++ source/javascripts/infographic/categories.js
@@ -3,9 +3,6 @@ var width = 350,
 height = 350,
 radius = Math.min(width, height) / 2;
 
-var color = d3.scale.ordinal()
-.range(["rgba(43, 84, 126,1.0)", "rgba(43, 84, 126,0.9)", "rgba(43, 84, 126,0.8)", "rgba(43, 84, 126,0.7)", "rgba(43, 84, 126,0.6)", "rgba(43, 84, 126,0.5)", "rgba(43, 84, 126,0.4)", "rgba(43, 84, 126,0.3)", "rgba(43, 84, 126,0.2)", "rgba(43, 84, 126,0.1)", "rgba(43, 84, 126,0.1)", "rgba(43, 84, 126,0.1)", "rgba(43, 84, 126,0.1)"]);
-
 var arc = d3.svg.arc()
 .outerRadius(radius - 5)
 .innerRadius(radius - 60);
@@ -14,19 +11,16 @@ var pie = d3.layout.pie()
 .sort(null)
 .value(function(d) { return d.percent; });
 
-
 var category_svg = d3.select("#info-category_counts").append("svg")
 .attr("width", width)
 .attr("height", height)
 .append("g")
 .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-
 //GROUP FOR CENTER TEXT
 var center_group = category_svg.append("svg:g")
 .attr("class", "center_group")
 .attr("transform", "translate(0,0)");
-
 
 var center_image = center_group.append("svg:image")
 .attr('x',-50)
@@ -48,13 +42,10 @@ var totalLabelCount = center_group.append("svg:text")
 .attr("text-anchor", "middle") // text-align: right
 .text("");
 
-
 var categories = function(data) {
   category_counts = data.categories.sort(function(a,b) { return b.percent - a.percent; });
 
-  // category_counts.forEach(function(d) {
-  //   d.days = +d.days;
-  // });
+  var color = d3.scale.linear().domain([0,data.categories.length]).range([0.1, 1]);
 
   var g = category_svg.selectAll(".arc")
   .data(pie(category_counts))
@@ -66,7 +57,7 @@ var categories = function(data) {
 
   g.append("path")
   .attr("d", arc)
-  .style("fill", function(d) { return color(d.data.key); });
+  .style("fill", function(d, i) { return "rgba(43, 84, 126, " + (1 - color(i)) + ")"; });
 
   var html = ich.categorylist({categories: category_counts.slice(0,10) });
   d3.select('#category-counts-list').html(html);
@@ -81,10 +72,6 @@ var categories = function(data) {
   });
 
 };
-
-
-
-
 
 function centerDisplayOver(cid, name, value) {
   d3.select('#' + cid).attr('class', 'arc_hover');
